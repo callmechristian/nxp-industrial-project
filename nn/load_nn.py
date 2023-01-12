a6219fc75406179fd076a9e5ec5f961b0ac6c714
citrovin <94009181+citrovin@users.noreply.github.com>
2023-01-12 15:21:04 +0100

    update loading nn

--- nn/load_nn.py
+++ nn/load_nn.py
@@ -5,8 +5,10 @@ import torch
 Find the final model trained by Unity:
 
 After running the training process, the model is saved in the directory:
-    "ml-agents/results/runid"
+    "ml-agents/results/<runid>/<model_name>/<model_name>_<#training_steps>.pt"
 
+
+Load the model from traning results directory and then you get the PyTorch model
 
 '''
 
